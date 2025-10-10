e721e6a393cd60bb85d8c83243ec1990950c2616
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-10-10 23:45:58 +0530

    Intermediate JS

--- 02-JavaScript/intermediate-JS/3.js
+++ 02-JavaScript/intermediate-JS/3.js
@@ -106,4 +106,135 @@ function loop(value){
   
 }
 
-loop(10)
+// loop(10)
+
+
+// -----------------------------------------------
+
+
+// Objects: objects in js can store values of more than one data type.
+
+let myObj = {
+  name : "Rahman",
+  email: "rahman69@gmail.com",
+  no: 8186978069
+}
+
+console.log(myObj.name, myObj.email, myObj.no);
+
+myObj.name = "Abdul rahman"
+
+console.log(myObj.name, myObj.email, myObj.no);
+
+myObj.address = "talab katta"
+console.log(myObj.name, myObj.email, myObj.no, myObj.address);
+
+delete myObj.no
+console.log(myObj.name, myObj.email, myObj.address);
+
+
+
+// there are mainly 11 methods in array which are very useful.
+
+// 1.object.keys(): 
+
+
+let newObj = {
+    myRollno: 66,
+    myClass: 'ece- 4th year!'
+}
+
+console.log(Object.keys(newObj));
+
+
+// 2.object.values(): 
+
+let newObj1 = {
+    myRollno: 66,
+    myClass: 'ece- 4th year!'
+}
+
+console.log(Object.values(newObj1));
+
+
+// 3.object.Entries(): 
+let newObj3 = {
+    myRollno: 24,
+    myClass: ' 4th year!'
+}
+console.log(Object.entries(newObj3));
+
+
+
+// 4.Object.assign(): 
+let newObj4 = {
+    myRollno: 66,
+    myClass: 'ece- 4th year!'
+}
+
+let newObj5 = {
+    myStreet: 'chandulal',
+    myLocation: 'baradari'
+}
+
+
+console.log(Object.assign(newObj4, newObj5));
+
+
+// 5.Object.Create(): 
+
+let myFilms = {
+    movie1: 'DDLJ',
+    movie2: 'Raees'
+}
+
+let srkMovies = Object.create(myFilms);
+console.log(srkMovies.movie1);
+
+
+// 6.Object.Freeze(): 
+
+let newObj7 = {
+    myRollno: 66,
+    myClass: 'ece- 4th year!'
+}
+
+console.log(Object.freeze(newObj7));
+
+newObj7.myWork = 'hw krna';
+console.log(newObj7);
+
+// object.isfrozen(): checks if an object is freeze or not!
+
+// 7.Object.fromEntries(): 
+
+let arr = [['myFname', 'Syed'], ['myLname', 'Abdul Rahman'], ['myLocation','Bahadurpura']]
+
+console.log(Object.fromEntries(arr));
+
+
+// 8.Object.is(): it is basically === like method 
+
+let a = 45;
+let b = 45;
+// console.log(Object.is(a ,b));
+
+// 8.Object.Seal(): 
+let newObj9 = {
+    myRollno: 66,
+    myClass: 'ece- 4th year!'
+}
+
+Object.seal(newObj9);
+newObj9.myCollege = "DCET";
+// console.log(newObj9);
+newObj9.myRollno = 5049;
+// console.log(newObj9);
+
+// object.isseal(): checks if an object is sealed or not: 
+
+
+// 10.Object.toString(): converts numbers into another system
+let smd = 123;
+let abd = smd.toString(2)
+console.log(abd);
