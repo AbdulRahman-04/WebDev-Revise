4bbfafe27215808811ebc4d0d51124b400c0b776
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-10-10 23:06:08 +0530

    Intermediate JS

--- 02-JavaScript/intermediate-JS/2.js
+++ 02-JavaScript/intermediate-JS/2.js
@@ -86,32 +86,80 @@ if (myCohort === "A24") {
   console.log("None of the above");
 }
 
-
-
 // // nested if else :
 
 let temp = 6;
 
-if (temp){
-  
-    console.log("Checking temperature");
+if (temp) {
+  console.log("Checking temperature");
 
-    if (temp > 0) {
-        console.log("Temperature is greater than 0 degree");
-        
-    } else if (temp >= 10) {
-        console.log("Temperature is greater than or equal to 10 degree");
-        
-    }  else if (temp >= 20) {
-        console.log("Temperature is greater than or equal to 20 degree");
-        
-    } else if (temp >= 30) {
-        console.log("Temperature is greater than or equal to 30 degree");
-        
-    } 
+  if (temp > 0) {
+    console.log("Temperature is greater than 0 degree");
+  } else if (temp >= 10) {
+    console.log("Temperature is greater than or equal to 10 degree");
+  } else if (temp >= 20) {
+    console.log("Temperature is greater than or equal to 20 degree");
+  } else if (temp >= 30) {
+    console.log("Temperature is greater than or equal to 30 degree");
+  }
 } else {
-    console.log("Temperature is less than 0 degree");
-    
+  console.log("Temperature is less than 0 degree");
 }
 
+// Loops: there are mainly two types of loops: entry controlled and exit controlled loops.
 
+// entry controlled loops: for loop  &  do while.
+// exit controlled loops: do while loop
+
+// for loop: this an entry controlled loop:
+
+// for (let i = 15; i >= 5; i--) {
+//   console.log(i);
+// }
+
+// while loop: this is also an entry controlled loop but with a different syntax
+
+// let j = 10;
+// while (j >= 1) {
+//   console.log(j);
+
+//   j--;
+// }
+
+// do while: it is an exit controlled loop
+
+// let k = 5;
+
+// do {
+//   console.log(k);
+  
+//   k++
+
+// } while(k<=15)
+
+
+// Switch Case : it is used to replace multiple if-else statements when we have a single variable to test against different values.
+
+// let day = 4;
+
+// switch(day){
+//   case 0 : {
+//     console.log("Today is Sunday");
+//     break;
+//   } 
+
+//   case 1: {
+//     console.log("Today is Monday");
+//     break;
+//   }
+
+//   case 2 : {
+//     console.log("Today is Tuesday");
+//     break
+//   }
+
+//   default : {
+//     console.log("Not a valid day!");
+    
+//   }
+// }
