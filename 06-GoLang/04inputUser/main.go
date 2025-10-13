48b2026064d8011d7a8bdd980aeec786687f9e1a
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2025-10-14 02:35:51 +0530

    GoLang Revision

---
+++ 06-GoLang/04inputUser/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+ 
+	// taking inputs from user
+
+	// two types : fmt.scan && bufio 
+
+	// fmt.Println("Type ur name")
+	// var name string
+
+	// fmt.Scan(&name)
+	// fmt.Printf("Your name is %s\n", name)
+
+
+	// bufio 
+
+	fmt.Println("Enter ur username")
+
+	reader := bufio.NewReader(os.Stdin)
+	userInput , _ := reader.ReadString('.')
+	fmt.Println(userInput)
+
+
+}
